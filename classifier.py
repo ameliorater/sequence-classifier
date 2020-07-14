7617714ff192caa49d32a67675550c75e2bbe0a5
Amelia <amelialeveille@gmail.com>
2020-07-14 15:43:40 -0500

    added PCA plot

--- classifier.py
+++ classifier.py
@@ -1,11 +1,14 @@
 import numpy as np
 from sklearn import svm, metrics
 from sklearn.model_selection import train_test_split, ShuffleSplit, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from matplotlib import pyplot as plt 
+
+# read and parse data
 
 filename = "splice.data"
-
 basesToValues = {'A': [1, 0, 0, 0], 'C': [0, 1, 0, 0], 'G': [0, 0, 1, 0], 'T': [0, 0, 0, 1], 'D': [0.3, 0, 0.3, 0.3], 'N': [0.25, 0.25, 0.25, 0.25], 'S': [0, 0.5, 0.5, 0], 'R': [0.5, 0, 0.5, 0]}
-
 rawData = np.loadtxt(filename, dtype=np.str, converters = {0: lambda s: s[0:-1]}, usecols=(0, 2))
 
 data = np.zeros((len(rawData), 60*4))
@@ -18,7 +21,13 @@ for r in range(0, len(rawData)):
         baseData = np.append(baseData, basesToValues[bases[i]])
     data[r] = baseData
 
-classifier = svm.SVC(C=1.5, gamma=0.02, random_state=42)
+data = StandardScaler().fit_transform(data)
+
+# train model & generate predictions
+
+# classifier = svm.SVC(C=1.5, gamma=0.02, random_state=42)
+classifier = svm.SVC(C=3.0, gamma=0.002, random_state=42)
+
 trainData, testData, trainLabels, testLabels = train_test_split(data, labels, test_size = 0.5, shuffle = True, random_state=42)
 classifier.fit(trainData, trainLabels)
 predicted = classifier.predict(testData)
@@ -29,8 +38,8 @@ print("Classification report for classifier %s:\n%s\n"
 
 # hyperparameter tuning
 
-# C_range = np.logspace(0, 1, base=10, num=5)
-# gamma_range = np.logspace(-2, -1, base=10, num=5)
+# C_range = np.logspace(-1, 1, base=10, num=5)
+# gamma_range = np.logspace(-4, -2, base=10, num=5)
 # param_grid = {'C': C_range, 'gamma': gamma_range}
 # cv = ShuffleSplit(n_splits=3, test_size=0.2, train_size=0.2)
 # cv.split(data)
@@ -39,3 +48,22 @@ print("Classification report for classifier %s:\n%s\n"
 
 # print("The best parameters are %s with a score of %0.2f"
 #       % (grid.best_params_, grid.best_score_))
+
+
+pca = PCA(n_components=2)
+pComps = pca.fit_transform(data)
+print(np.shape(pComps))
+final = np.append(pComps, labels)
+print(np.shape(final))
+
+labelsToInts = {b'EI': 0, b'IE': 1, b'N': 2}
+labelInts = []
+for i, label in enumerate(labels):
+    labelInts.append(labelsToInts[label])
+
+plt.scatter(pComps[:,0], pComps[:,1], c=labelInts)
+# plt.plot(pComps, 'o')
+plt.title("PCA 2D Projection of Sequence Data")
+plt.xlabel("First Principal Component")
+plt.ylabel("Second Principal Component")
+plt.show()
